92c0a32a51c0bc0bd2294f0d99bd50c1ab70a96f
lui2mi <luimi@live.com>
2022-02-11 22:57:56 -0500

    empty state favoritos

---
+++ client/src/app/components/components.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+import { EmptyStateComponent } from './empty-state/empty-state.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+  ],
+  declarations: [EmptyStateComponent],
+  exports: [EmptyStateComponent]
+})
+export class ComponentsModule {}
