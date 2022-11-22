be1ed7b162b6d7d0a5b01af64b366eb9e7db4c2a
luis meza <luimi@live.com>
2022-11-22 11:54:21 -0500

    arreglo yugenmangas

--- server/manga/yugenmangas.js
+++ server/manga/yugenmangas.js
@@ -3,132 +3,110 @@ const axios = require("axios");
 const root = "https://yugenmangas.com";
 var FormData = require("form-data");
 
+/*
+
+  __  __       _       
+ |  \/  |     (_)      
+ | \  / | __ _ _ _ __  
+ | |\/| |/ _` | | '_ \ 
+ | |  | | (_| | | | | |
+ |_|  |_|\__,_|_|_| |_|
+                       
+                       
+{title,url,poster}
+*/
 const main = (html) => {
-  let result = [];
   let $ = cheerio.load(html);
-  $(".site-content")
-    .find(".series")
-    .each((index, element) => {
-      let a = $(element).find("a");
-      let url = a.attr("href");
-      let e = $(element);
-      let title = e.parent().find("h5").text();
-      let poster = a.find("img").attr("src");
-      result.push({ title, url, poster });
-    });
+  let result = [];
+  $(".series_item__SeriesCard-sc-t4shg7-0").each((i,e)=> {
+    let parent = $(e).parent();
+    let poster = parent.find(".series_item__SeriesCard-sc-t4shg7-0").find("a").find("img").attr("src");
+    let title = parent.find(".series_item__SeriesTitle-sc-t4shg7-5").text().trim().replace(/ /g, ' ').replace('\n', ' ');
+    let url = root+parent.find(".series_item__SeriesContent-sc-t4shg7-2").find("a").attr("href");
+    result.push({title,url,poster});
+  })
   return result;
 };
+/*
+
+   _____                     _     
+  / ____|                   | |    
+ | (___   ___  __ _ _ __ ___| |__  
+  \___ \ / _ \/ _` | '__/ __| '_ \ 
+  ____) |  __/ (_| | | | (__| | | |
+ |_____/ \___|\__,_|_|  \___|_| |_|
+                                   
+                                   
+{title,url,poster}
+*/
 const search = async (html) => {
-  let result = [];
   let $ = cheerio.load(html);
-  $(".c-tabs-item")
-    .find(".c-tabs-item__content")
-    .each((index, element) => {
-      let a = $(element).find(".tab-thumb").find("a");
-      let url = a.attr("href");
-      let e = $(element);
-      let title = e.find(".tab-summary").find("h3").text();
-      let poster = a.find("img").attr("data-src");
-      result.push({ title, url, poster });
-    });
+  let result = [];
+  $(".series_item__SeriesCard-sc-t4shg7-0").each((i,e)=> {
+    let parent = $(e).parent();
+    let poster = parent.find(".series_item__SeriesCard-sc-t4shg7-0").find("a").find("img").attr("src");
+    let title = parent.find(".series_item__SeriesTitle-sc-t4shg7-5").text().trim().replace(/ /g, ' ').replace('\n', ' ');
+    let url = root+parent.find(".series_item__SeriesContent-sc-t4shg7-2").find("a").attr("href");
+    result.push({title,url,poster});
+  })
   return result;
 };
+/*
+
+  ______       _               _           
+ |  ____|     (_)             | |          
+ | |__   _ __  _ ___  ___   __| | ___  ___ 
+ |  __| | '_ \| / __|/ _ \ / _` |/ _ \/ __|
+ | |____| |_) | \__ \ (_) | (_| |  __/\__ \
+ |______| .__/|_|___/\___/ \__,_|\___||___/
+        | |                                
+        |_|                                
+{title,poster,episodes:[{title,url}],categories:[string],extras:[{title,content}]}
+*/
 const episodes = async (html) => {
+  let $ = cheerio.load(html);
   let episodes = [];
   let categories = [];
   let extras = [];
-  let $ = cheerio.load(html);
-  let poster = undefined;
-  let title = $(".post-title").find("h1").text().trim();
-  let synopsis = $(".summary__content").find("p").text();
-  let currentUrl = "";
-  $("meta").each((index, element) => {
-    let e = $(element);
-    if (e.attr("property") === "og:image") {
-      poster = e.attr("content");
-    }
-    if (e.attr("property") === "og:url") {
-      currentUrl = e.attr("content");
-    }
+  let poster = $(".series__ThumbImg-sc-v2h5e-0").attr("src");
+  let title = $(".series-title").find("h1").text().trim().replace('\n', ' ');
+  let synopsis = $(".description-container").text().trim().replace('\n', ' ').replace(/\s+/g, ' ');
+  $(".tags-container").find("span").each((i,e)=> {
+    categories.push($(e).text());
   });
-  $(".summary_content")
-    .find(".post-content")
-    .find(".post-content_item")
-    .each((index, element) => {
-      let e = $(element);
-      let _title = e.find(".summary-heading").text().trim();
-      let content = e.find(".summary-content").text().replace(title, "").trim();
-      if (_title === "Genero(s)") {
-        let list = content.split(",");
-        list.forEach((text) => {
-          categories.push(text.trim());
-        });
-      } else {
-        extras.push({
-          title: _title,
-          content: content,
-        });
-      }
-    });
-  $(".summary_content")
-    .find(".post-status")
-    .find(".post-content_item")
-    .each((index, element) => {
-      let e = $(element);
-      let title = e.find(".summary-heading").text().trim();
-      let content = e.find(".summary-content").text().trim();
-      extras.push({
-        title: title,
-        content: content,
-      });
-    });
-  var config = {
-    method: "post",
-    url: `${currentUrl}ajax/chapters/`,
-  };
-  try {
-    let response = await axios(config);
-    $ = cheerio.load(response.data);
-    $(".version-chap")
-      .find("a")
-      .each((index, element) => {
-        let a = $(element);
-        episodes.push({
-          title: a.find(".chapter-manhwa-title").text(),
-          url: a.attr("href"),
-        });
-      });
-  } catch (e) {}
+  $(".chapters-list-single").find("a").each((i,e) => {
+    let a = $(e);
+    let url = root+a.attr("href");
+    let title = a.find(".name").text().replace('\n', ' ').replace(/\s+/g, ' ');
+    episodes.push({title,url})
+  });
   return { poster, title, synopsis, categories, extras, episodes };
 };
-const episode = (html) => {
-  let pages = [];
-  let next = undefined;
-  let previous = undefined;
+/*
+
+  ______       _               _      
+ |  ____|     (_)             | |     
+ | |__   _ __  _ ___  ___   __| | ___ 
+ |  __| | '_ \| / __|/ _ \ / _` |/ _ \
+ | |____| |_) | \__ \ (_) | (_| |  __/
+ |______| .__/|_|___/\___/ \__,_|\___|
+        | |                           
+        |_|                           
+{title,pages:[string],next,previous,episodes}
+*/
+const episode = async (html) => {
   let $ = cheerio.load(html);
-  let title = $("#chapter-heading").text();
-  let episodes = undefined;
-  $(".breadcrumb:first")
-    .find("li")
-    .each((index, element) => {
-      if (index === 1) {
-        let a = $(element).find("a");
-        episodes = a.attr("href");
-      }
-    });
-  $(".reading-content")
-    .find("img")
-    .each((index, element) => {
-      let e = $(element);
-      pages.push(e.attr("data-src").trim());
-    });
-  let nav = $(".select-pagination");
-  previous = nav.find(".nav-previous").find("a").attr("href");
-  next = nav.find(".nav-next").find("a").attr("href");
-  if(episodes === next){
-    next = undefined;
-  }
-  return { title, pages, next, previous, episodes };
+  let title = $(".c-dsYLkv").text().trim().replace('\n', ' ').replace(/\s+/g, ' ');
+  let episodes = root+$(".c-dsYLkv").find("a").attr("href");
+  let previous = root+$(".prev-chap").find("a").attr("href");
+  let next = root+$(".next-chap").find("a").attr("href");
+  let pages = [];
+  let data = JSON.parse($("#__NEXT_DATA__").text());
+  let response = await axios.get("https://api.yugenmangas.com/series/chapter/"+data.props.pageProps.data.id);
+  response.data.content.images.forEach((image)=> {
+    pages.push("https://api.yugenmangas.com/"+image);
+  })
+  return {title,episodes,previous,next,pages};
 };
 
 module.exports = {
