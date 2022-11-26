e87d0d5950cb06ef56ef097852bb862efab95aca
luis meza <luimi@live.com>
2022-11-25 19:10:33 -0500

    ajuste template anime

--- server/anime/template.js
+++ server/anime/template.js
@@ -1,10 +1,23 @@
 const cheerio = require('cheerio');
-const root = "URL"
+const root = "URL";
+/*
+
+  __  __       _       
+ |  \/  |     (_)      
+ | \  / | __ _ _ _ __  
+ | |\/| |/ _` | | '_ \ 
+ | |  | | (_| | | | | |
+ |_|  |_|\__,_|_|_| |_|
+                       
+                       
+{title,url,poster}
+*/
 const main = (html) => {
-    let result = []
-    let $ = cheerio.load(html)
-    $('.class').each((index, element) => {
-        let a = $(element)
+    let $ = cheerio.load(html);
+
+    let result = [];
+    $('.class').each((i, e) => {
+        let a = $(e)
         let url = a.attr('href')
         let title = ""
         let poster = a.find('img').attr('src')
@@ -12,11 +25,24 @@ const main = (html) => {
     });
     return result;
 };
+/*
+
+   _____                     _     
+  / ____|                   | |    
+ | (___   ___  __ _ _ __ ___| |__  
+  \___ \ / _ \/ _` | '__/ __| '_ \ 
+  ____) |  __/ (_| | | | (__| | | |
+ |_____/ \___|\__,_|_|  \___|_| |_|
+                                   
+                                   
+{title,url,poster}
+*/
 const search = (html) => {
-    let result = []
-    let $ = cheerio.load(html)
-    $('.class').each((index, element) => {
-        let a = $(element).find('a')
+    let $ = cheerio.load(html);
+
+    let result = [];
+    $('.class').each((i, e) => {
+        let a = $(e).find('a')
         let url = a.attr('href')
         let title = $(element).find('a').text()
         let poster = $(element).attr('data-setbg')
@@ -24,41 +50,67 @@ const search = (html) => {
     });
     return result;
 };
+/*
+
+  ______       _               _           
+ |  ____|     (_)             | |          
+ | |__   _ __  _ ___  ___   __| | ___  ___ 
+ |  __| | '_ \| / __|/ _ \ / _` |/ _ \/ __|
+ | |____| |_) | \__ \ (_) | (_| |  __/\__ \
+ |______| .__/|_|___/\___/ \__,_|\___||___/
+        | |                                
+        |_|                                
+{title,poster,episodes:[{title,url}],categories:[string],extras:[{title,content}]}
+*/
 const episodes = (html) => {
-    let episodes = []
-    let categories = []
-    let extras = []
-    let $ = cheerio.load(html)
-    let poster = $('.class').attr('data-setbg')
-    let title = $('.class').text()
-    let synopsis = $('.class').text()
-    $('.class').each((index, element) => {
+    let $ = cheerio.load(html);
+
+    let episodes = [];
+    let categories = [];
+    let extras = [];
+    let poster = $('.class').attr('data-setbg');
+    let title = $('.class').text();
+    let synopsis = $('.class').text();
+    $('.class').each((i, e) => {
         extras.push({ title: "", content: "" });
     });
-    $('.class').each((index, element) => {
-        categories.push($(element).text())
+    $('.class').each((i, e) => {
+        categories.push($(e).text())
     });
-    $('.class').each((index, element) => {
+    $('.class').each((i, e) => {
         episodes.push({ title:"", url:"" })
     });
     return { poster, title, synopsis, categories, extras, episodes };
 };
+/*
+
+  ______       _               _      
+ |  ____|     (_)             | |     
+ | |__   _ __  _ ___  ___   __| | ___ 
+ |  __| | '_ \| / __|/ _ \ / _` |/ _ \
+ | |____| |_) | \__ \ (_) | (_| |  __/
+ |______| .__/|_|___/\___/ \__,_|\___|
+        | |                           
+        |_|                           
+{title,links:[string],strams:[string],next,previous,episodes}
+*/
 const episode = (html) => {
-    let links = []
-    let streams = []
-    let next = undefined
-    let previous = undefined
     let $ = cheerio.load(html)
+
+    let links = [];
+    let streams = [];
+    let next = undefined;
+    let previous = undefined;
     let title = $('.class').text();
-    let episodes = undefined
-    $(".class").each((index, element) => {
-        let url = $(element).attr('href')
+    let episodes = undefined;
+    $(".class").each((i, e) => {
+        let url = $(e).attr('href')
         links.push(url);
     });
 
-    $("script").each((index, element) => {
-        let e = $(element)
-        streams.push(e.text())
+    $("script").each((i, e) => {
+        let el = $(e)
+        streams.push(el.text())
     });
     return { title, links, streams, next, previous , episodes};
 };
