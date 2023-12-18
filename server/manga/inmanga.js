8a275275a4926ab1cb76a3d3c53e5fea14bc44be
Luis meza <luimi@live.com>
2023-12-18 16:16:01 -0500

    agregado chapter a inmanga

--- server/manga/inmanga.js
+++ server/manga/inmanga.js
@@ -7,22 +7,23 @@ const main = (html) => {
     $('.list-group-item').each((index, element) => {
         let a = $(element)
         let url = `${root}${a.attr('href')}`.encode()
-        let title = `${a.find('.text-primary').text().clearSpaces()} - ${a.find('.recent-chapter-container-footer').find('strong').text().clearSpaces()}`
+        let title = a.find('.text-primary').text().clearSpaces()
+        let chapter = a.find('.recent-chapter-container-footer').find('strong').text().clearSpaces().split(" ").pop()
         let poster = a.find('img').attr('src')
-        result.push({ title, url, poster })
+        result.push({ title, url, poster, chapter })
     });
     return result;
 };
 const search = async (html) => {
     let result = []
-    try{
+    try {
         let response = await axios.get(`https://inmanga.com/manga/GetQuickSearch?name=${html.query}`);
         let data = JSON.parse(response.data.data);
         data.result.forEach(item => {
-            result.push({ title:item.Name, url:`${root}/ver/manga/${item.Name.replace(/\s/g,"-")}/${item.Identification}`.encode(), poster:item.ThumbnailPath })
+            result.push({ title: item.Name, url: `${root}/ver/manga/${item.Name.replace(/\s/g, "-")}/${item.Identification}`.encode(), poster: item.ThumbnailPath })
         });
-    }catch(e){
-        return {success:false, error:e}
+    } catch (e) {
+        return { success: false, error: e }
     }
     return result;
 };
@@ -38,20 +39,20 @@ const episodes = async (html) => {
     let synopsis = texts.find('.panel-body').text().clearSpaces()
     $('.list-group-item').each((index, element) => {
         let a = $(element)
-        let texts = a.text().clearSpaces().replace(/\s+/g,":").split(":")
-        if(texts.length==2)
+        let texts = a.text().clearSpaces().replace(/\s+/g, ":").split(":")
+        if (texts.length == 2)
             extras.push({ title: texts[1], content: texts[0] });
     });
     let mangaId = $('#Identification').attr('value');
     let posterSegments = posterPath.split("/");
-    try{
+    try {
         let response = await axios.get(`https://inmanga.com/chapter/getall?mangaIdentification=${mangaId}`)
         let data = JSON.parse(response.data.data);
-        data.result.sort((a,b)=>{return a.Number < b.Number?1:-1})
+        data.result.sort((a, b) => { return a.Number < b.Number ? 1 : -1 })
         data.result.forEach((manga) => {
-            episodes.push({ title:`Capítulo ${manga.Number}`, url:`${root}/ver/manga/${posterSegments[3]}/${manga.Number}/${manga.Identification}`.encode() })
+            episodes.push({ title: manga.Number, url: `${root}/ver/manga/${posterSegments[3]}/${manga.Number}/${manga.Identification}`.encode() })
         });
-    }catch(e){}
+    } catch (e) { }
     return { poster, title, synopsis, categories, extras, episodes };
 };
 const episode = async (html) => {
@@ -59,35 +60,37 @@ const episode = async (html) => {
     let next = undefined
     let previous = undefined
     let $ = cheerio.load(html)
-    let title = $('.ChapterDescriptionContainer').find('h1').text().clearSpaces();
+    let name = $('.ChapterDescriptionContainer').find('h1').text().clearSpaces();
+    let chapter = name.split(" ").pop()
+    let title = name.replace(` Capítulo ${chapter}`,"").trim()
     let episodes = undefined
     let pathSegmented = undefined
     $("meta").each((index, element) => {
         let e = $(element)
-        if(e.attr('property') === 'og:url'){
+        if (e.attr('property') === 'og:url') {
             pathSegmented = e.attr('content').split("/")
         }
     });
-    try{
+    try {
         let response = await axios.get(`https://inmanga.com/chapter/chapterIndexControls?identification=${pathSegmented[7]}`)
         $ = cheerio.load(response.data)
-        $(".ImageContainer").each((index,element) => {
+        $(".ImageContainer").each((index, element) => {
             let e = $(element)
             pages.push(`https://pack-yak.intomanga.com/images/manga/${pathSegmented[5]}/chapter/${pathSegmented[6]}/page/${e.attr('data-pagenumber')}/${e.attr('id')}`)
         });
         episodes = `${root}${$('.list-group-item').attr('href')}`.encode();
-        $('#ChapList').find('option').each((index,element) => {
+        $('#ChapList').find('option').each((index, element) => {
             let e = $(element)
-            if(parseInt(e.text().clearSpaces()) === parseInt(pathSegmented[6])-1){
+            if (parseInt(e.text().clearSpaces()) === parseInt(pathSegmented[6]) - 1) {
                 previous = `${root}/ver/manga/${pathSegmented[5]}/${e.text().clearSpaces()}/${e.attr('value')}`.encode()
             }
-            if(parseInt(e.text().clearSpaces()) === parseInt(pathSegmented[6])+1){
+            if (parseInt(e.text().clearSpaces()) === parseInt(pathSegmented[6]) + 1) {
                 next = `${root}/ver/manga/${pathSegmented[5]}/${e.text().clearSpaces()}/${e.attr('value')}`.encode()
             }
         })
-    }catch(e){}
+    } catch (e) { }
 
-    return { title, pages, next, previous, episodes };
+    return { title, pages, next, previous, episodes, chapter };
 };
 
 module.exports = {
