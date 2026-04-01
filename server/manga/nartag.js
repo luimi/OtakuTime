af0ab60fe2c024541b09d95c9ce2575e74aec467
Luis Meza <luimi@live.com>
2026-03-31 20:48:39 -0500

    arreglo nartag

--- server/manga/nartag.js
+++ server/manga/nartag.js
@@ -1,56 +1,74 @@
 const cheerio = require('cheerio');
-const root = "https://nartag.com"
+const root = "https://nartag.com/"
 const main = (html) => {
     let $ = cheerio.load(html)
     let result = []
-    $('#loop-content').find(".page-item-detail").each((index, element) => {
-        let e = $(element)
-        let a = e.find("a")
-        let url = e.find('.chapter-item').first().find('a').attr('href')
-        let title = e.find('.post-title').text().clearSpaces() + " - " + e.find('.chapter-item').first().text().clearSpaces()
-        let poster = process.env.SERVER + "/image?url=" + e.find('img').attr('src')
-        if(url!=="#" && e.find('img').attr('src') && !e.text().toLowerCase().includes("novela"))
-        result.push({ title, url, poster })
+    $('.page-item-detail').each((index, element) => {
+
+        let e = $(element);
+        let a = e.find("a");
+        let btnLink = e.find('.btn-link').first()
+        let url = btnLink.attr('href').encode();
+        let poster = a.find('img').attr('src');
+        let name = e.find('h3').find('a').text();
+        let cap = btnLink.text()
+        let title = name.clearSpaces() + ' - ' + cap.clearSpaces()
+        if (url !== "Iw==") result.push({ title, poster, url })
     });
     return result;
 };
 const search = (html) => {
     let result = []
     let $ = cheerio.load(html)
-    $('.c-tabs-item__content').each((index, element) => {
-        let e = $(element)
-        let a = e.find('.post-title').find('a')
-        let url = a.attr('href')
-        let title = a.text().clearSpaces()
-        let poster = process.env.SERVER + "/image?url=" + e.find('img').attr('src')
-        if(e.find('img').attr('src'))
-        result.push({ title, url, poster })
+    $('.row.c-tabs-item__content').each((index, element) => {
+
+        let e = $(element);
+        let a = e.find("a");
+
+        let url = a.attr('href').encode();
+        let poster = e.find('a').find('img').attr('src');
+        let title = e.find('h3').text();
+
+        result.push({ title, poster, url })
     });
     return result;
 };
-const episodes = (html) => {
+const episodes = async (html) => {
     let episodes = []
     let categories = []
     let extras = []
     let $ = cheerio.load(html)
+
     let poster = $('.summary_image').find('img').attr('src')
-    let title = $('.manga-title').find('h1').text().clearSpaces()
+    let title = $('#manga-title').find('h1').text().clearSpaces()
+
     let synopsis = ""
     $('.post-content_item').each((index, element) => {
         let e = $(element);
         let _title = e.find('.summary-heading').text().clearSpaces()
-        let _content = e.find('.summary-content').text().replace(title,"").clearSpaces()
-        if(title === "Genre(s)") {
-          categories = _content.replace(/\s/g,"").split(",")
-        } else {
-          extras.push({ title: _title, content: _content });
-        }
-        
+        let _content = e.find('.summary-content').text().replace(title, "").clearSpaces()
+        if (_title && _content) extras.push({ title: _title, content: _content });
+
     });
-    $('.wp-manga-chapter').each((index, element) => {
-        let a = $(element).find('a')
-        episodes.push({ title:a.text().clearSpaces(), url:a.attr('href') })
-    });
+
+    const scriptContent = $('#madara-js-js-extra').html();
+    const match = scriptContent.match(/var madara = (.*?);/s);
+
+    if (match) {
+        const jsonString = match[1];
+        const obj = JSON.parse(jsonString);
+        let chapters = await fetch(`${obj.current_url}/ajax/chapters/?t=1`, { method: "POST" });
+        chapters = await chapters.text()
+        chapters = cheerio.load(chapters)
+        chapters('.free-chap').each((index, element) => {
+            let a = $(element).find('a')
+            episodes.push({ title: a.text().clearSpaces(), url: a.attr('href').encode() })
+        });
+    } else {
+        console.log("No se pudo convertir el objeto");
+    }
+
+
     return { poster, title, synopsis, categories, extras, episodes };
 };
 const episode = (html) => {
@@ -62,12 +80,12 @@ const episode = (html) => {
     let episodes = undefined
     $(".reading-content").find('img').each((index, element) => {
         let e = $(element)
-        pages.push(e.attr('data-src').clearSpaces())
+        pages.push(e.attr('src').clearSpaces())
     });
-    $(".breadcrumb").find("a").each((index,element) => {
+    $(".breadcrumb").find("a").each((index, element) => {
         let a = $(element)
-        if(index === 1){
-          episodes = a.attr('href')
+        if (index === 1) {
+            episodes = a.attr('href')
         }
     });
     previous = $(".nav-previous").find("a").attr('href')
